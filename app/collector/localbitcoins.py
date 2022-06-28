1f2525e6c93aab8bce4e9499593dc1f48f8fc0a5
mtroshin <m.troshin@chatme.ai>
2022-06-28 23:29:47 +0300

    fix banks reading

--- app/collector/localbitcoins.py
+++ app/collector/localbitcoins.py
@@ -51,7 +51,7 @@ class LocalbitcoinsPriceCollector(Collector):
             i = 0
             banks = []
             while i < len(names):
-                banks.append((self.__browser.find_element((By.XPATH, '/html/body/div[4]/table/tbody/tr[' + str(i + 2) + ']/td[2]'))).text)
+                banks.append((self.__browser.find_element(By.XPATH, '/html/body/div[4]/table/tbody/tr[' + str(i + 2) + ']/td[2]')))
                 i += 1
             i = 0
 
