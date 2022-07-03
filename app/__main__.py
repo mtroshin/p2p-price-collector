49d9f5296fd94fab05a75b410ce0e99306e52592
mtroshin <m.troshin@chatme.ai>
2022-07-03 16:07:23 +0300

    Refactor main, use env for database configuration

--- app/__main__.py
+++ app/__main__.py
@@ -1,16 +1,16 @@
-
-from selenium.webdriver import ChromeOptions, Chrome
-from webdriver_manager.chrome import ChromeDriverManager
-import psycopg2
 import datetime
+import logging
+from multiprocessing.sharedctypes import Value
+import os
+
+
+
+from selenium.webdriver import ChromeOptions, Remote
 from app.config import host, user, password, db_name
 
 from app.collector.huobi import HuobiPriceCollector
 from app.collector.localbitcoins import LocalbitcoinsPriceCollector
-import logging
-import os
 
-from selenium.webdriver import ChromeOptions, Remote, Chrome
 
 def create_driver():
     command_executor = os.getenv("COMMAND_EXECUTOR")
@@ -24,56 +24,66 @@ def create_driver():
     return driver
 
 
+def create_db_conn():
+    host = os.getenv("DB_HOST")
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    database = os.getenv("DB_DATABASE", "postgres")
+    port = int(os.getenv("DB_PORT", "5432"))
+
+    if not host or not user or not password:
+        raise ValueError("DB_HOST, DB_USER, DB_PASSWORD are required for start")
+
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=port,
+    )
+
+    connection.autocommit = True
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS bits (
+                ts INTEGER,
+                exchange VARCHAR(255),
+                min_amount INTEGER NOT NULL,
+                max_amount INTEGER NOT NULL,
+                price INTEGER NOT NULL,
+                currency VARCHAR(3),
+                bank VARCHAR(255),
+                seller_id VARCHAR);"""
+        )
 
 if __name__ == '__main__':
     logging.basicConfig(level=getattr(logging, os.getenv("LOGGING_LEVEL", "info").upper()))
     
-    ct = datetime.datetime.now()
-    tss = ct.timestamp()
+    t = datetime.datetime.now().timestamp()
 
     driver = create_driver()
+    db_conn = create_db_conn()
     try:
             collector = LocalbitcoinsPriceCollector(driver)
             orders = list(collector.collect())
-            print(orders[0])
-            connection = psycopg2.connect(
-            host = host,
-            user = user,
-            password = password,
-            database = db_name
-            )
-            connection.autocommit = True
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    """CREATE TABLE IF NOT EXISTS bits (
-                        ts INTEGER,
-                        exchange VARCHAR(255),
-                        min_amount INTEGER NOT NULL,
-                        max_amount INTEGER NOT NULL,
-                        price INTEGER NOT NULL,
-                        currency VARCHAR(3),
-                        bank VARCHAR(255),
-                        seller_id VARCHAR);"""
-                )
-            j = 0
-            for i in orders:
-                with connection.cursor() as cursor:
-                    cursor.execute("INSERT INTO bits VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (tss, 'huobi', orders[j][0], orders[j][1], orders[j][3], orders[j][2], orders[j][4], orders[j][5]))
-                    j += 1
-            j = 0
-            i = 0
+
+            with db_conn.cursor() as cursor:
+                for order in orders:
+                    cursor.execute(
+                        "INSERT INTO bits VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
+                        (t, 'local', order.min_amount, order.max_amount, order.price, order.currency, order.bank, order.seller_id)
+                    )
+
             collector = HuobiPriceCollector(driver)
             orders = list(collector.collect())
-            for i in orders:
-                with connection.cursor() as cursor:
-                    cursor.execute("INSERT INTO bits VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (tss, 'huobi', orders[j][0], orders[j][1], orders[j][3], orders[j][2], orders[j][4], orders[j][5]))
-                    j += 1
 
-    
+            with db_conn.cursor() as cursor:
+                for order in orders:
+                    cursor.execute(
+                        "INSERT INTO bits VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
+                        (t, 'huobi', order.min_amount, order.max_amount, order.price, order.currency, order.bank, order.seller_id)
+                    )
 
-    finally:
-        
+    finally:        
         driver.close()
-        if connection:
-            connection.close()
-            print("[INFO] PostgreSQL connection closed")
+        db_conn.close()
